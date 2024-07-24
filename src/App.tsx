d05b7d18d934d47190d4a4f5c28c191593ee5048
fadrian06 <franyeradriansanchez@gmail.com>
2024-07-24 15:19:42 -0400

    Changed App.jsx -> App.tsx in home page

--- src/App.tsx
+++ src/App.tsx
@@ -21,7 +21,7 @@ export default function App() {
           count is {count}
         </button>
         <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
+          Edit <code>src/App.tsx</code> and save to test HMR
         </p>
       </div>
       <p className='read-the-docs'>Click on React logo to learn more</p>
